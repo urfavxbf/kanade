bd2752e150298a87fa1738e4158cdb49c381c3c8
UrFavXBF <urfavxbf@gmail.com>
2026-09-02 11:24:37 +0800

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") apply false version "8.11.0"
-    id("com.android.library") apply false version "8.11.0"
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
